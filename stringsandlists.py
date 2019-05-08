a011f113f23533bd9c4cf5e21fab565cc2c39d12
Noel Mendoza <nmendozavd@gmail.com>
2019-05-08 05:42:24 -0700

    updated with readme

--- stringsandlists.py
+++ stringsandlists.py
@@ -1,3 +1,5 @@
+
+"""
 #find and replace
 string = "It's thanksgiving day. It's my birthday, too!"
 print string
@@ -26,3 +28,40 @@ print "first list", first_list
 print "second_list", second_list
 second_list.insert(0,first_list)
 print second_list
+
+"""
+
+# Find and replace
+
+words = "It's thanksgiving day. It's my birthday too!"
+print words
+print words.find('day')
+print words.replace('day', 'month')
+
+#Min and Max
+x = [2,43-2,7,12,98]
+print min(x)
+print max(x)
+
+#first and last
+x = ["hello",2,54,-2,7,12,98,"world"]
+print x[0]
+print x[len(x)-1]
+
+new_list = [x[0],x[len(x)-1]]
+print new_list
+
+
+#New List
+x = [19,2,54,-2,7,12,98,32,10,-3,6]
+print x
+print x.sort()
+print x
+print x[:len(x)//2]
+new_list2 = [x[:len(x)//2], 10, 12, 19, 32, 54, 98]
+print new_list2
+
+
+
+
+
